0d8aef3a45cb21e9fef92915a32105812bd1f695
SkyLineCoderz-Raheel <muhammadraheel6240@gmail.com>
2025-01-09 00:19:41 +0500

    Initial commit

---
+++ android/app/src/main/kotlin/com/skylinecoderz/why_me/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.skylinecoderz.why_me
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:get/get.dart';
+import 'package:why_me/views/screens/screen_splash.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return ScreenUtilInit(
+      designSize: const Size(390, 844),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      builder: (_ , child) {
+        return GetMaterialApp(
+          debugShowCheckedModeBanner: false,
+          title: 'First Method',
+          theme: ThemeData(
+            primarySwatch: Colors.blue,
+            textTheme: Typography.englishLike2018.apply(fontSizeFactor: 1.sp),
+          ),
+          home: child,
+        );
+      },
+      child:ScreenSplash() ,
+    );
+  }
+}
+
+/// Project structure
+/// MVC(model view(screens-layouts) controller)
+/// MVVM
+/// FLutter screen utils responsiveness
+/// height 10.h
+/// width 10.w
+/// radius 6.r
+/// font 12.sp
